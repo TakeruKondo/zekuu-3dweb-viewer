5a6d559b245d3115a4b8611f318b7a4d1c097f65
Takeru Kondo <codeit910@yahoo.com>
2025-04-24 16:31:54 -0700

    Include all recent changes for Render deployment

--- entry.js
+++ entry.js
@@ -10,11 +10,21 @@ async function loadMainScript() {
 function createLoader() {
   const loaderDiv = document.createElement("div");
   loaderDiv.classList.add("loader-ui");
-  const loaderGif = document.createElement("img");
-  loaderGif.src = globeGif;
-  loaderDiv.appendChild(loaderGif);
+  const loaderLogo = document.createElement("img");
+  loaderLogo.src = "images/zekuu-creative-logo.png";
+  loaderDiv.appendChild(loaderLogo);
 
-  loaderDiv.style.backgroundColor = "#383F4D";
+  const loadingTextElement = document.createElement("div");
+  loadingTextElement.textContent = "Loading ZEKUU 3D Viewer...";
+  loadingTextElement.style.color = "#000000";
+  loadingTextElement.style.position = "absolute";
+  loadingTextElement.style.top = "calc(50% + 50px)";
+  loadingTextElement.style.left = "50%";
+  loadingTextElement.style.transform = "translateX(-50%)";
+  loadingTextElement.style.fontFamily = "sans-serif";
+  loaderDiv.appendChild(loadingTextElement);
+
+  loaderDiv.style.backgroundColor = "#FFFFFF";
   document.body.appendChild(loaderDiv);
 
   loadMainScript()
